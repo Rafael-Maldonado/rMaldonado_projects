2a77b838f7e4217891b887efdeab3a1c39208869
Rafael-Maldonado <94449249+Rafael-Maldonado@users.noreply.github.com>
2021-11-16 04:00:39 -0800

    Add files via upload

---
+++ Hello World.py
@@ -0,0 +1,19 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("Hello World")
+Hello World
+print("Welcome to the world of Python!")
+Welcome to the world of Python!
+def SayHellow(name):
+    print("Hello",name)
+    print("Welcome to Python Tutorials")
+
+    
+SayHwllow("Rafael")
+Traceback (most recent call last):
+  File "<pyshell#6>", line 1, in <module>
+    SayHwllow("Rafael")
+NameError: name 'SayHwllow' is not defined. Did you mean: 'SayHellow'?
+SayHellow("Rafael")
+Hello Rafael
+Welcome to Python Tutorials
